f4c61c80968a9f6a613422ce11acaa8fff3ad4ae
LEO-KWON1 <kojkhj614@naver.com>
2025-04-24 13:03:20 +0800

    Switch to HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
 import { AuthProvider } from './context/AuthContext'
 import MainLayout from './components/layout/MainLayout'
